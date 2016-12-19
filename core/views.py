703258dc72952a190fe215a6196c695a73c4e743
fernando <fjanuarioazevedo@gmail.com>
2016-12-19 00:13:47 -0300

    subindo heroku

--- core/views.py
+++ core/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+# coding=utf-8
 
-# Create your views here.
+from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-	return render(request,'index.html')
+    return render(request, 'index.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def product_list(request):
+    return render(request, 'product_list.html')
+
+
+def product(request):
+    return render(request, 'product.html')
